19657c19275fddaf79e6dd0ddf130e7a713cd43a
Mike Treadway <mike@Mikes-MacBook-Pro.local>
2018-01-17 16:37:11 -0600

    Updated help messages.

--- commands/set/AskTapState.js
+++ commands/set/AskTapState.js
@@ -9,7 +9,7 @@ AskTapState.prototype.process = async function(message) {
     var tap = _.toNumber(message.content);
     if (!_.isNaN(tap)) {
         if (tap < 1 || tap > 8){
-            message.channel.send("The tap number must be between 1 and 8. Please try again:");
+            message.channel.send("The tap number must be between 1 and 8. Please try again or type 'cancel':");
         } else{
             return UploadChangesState.new(this._beer, tap);            
         }
@@ -19,7 +19,7 @@ AskTapState.prototype.process = async function(message) {
             message.channel.send("User cancelled");
             return;
         } else{
-            message.channel.send("That wasn't a tap number. Please try again:");            
+            message.channel.send("That wasn't a tap number. Please try again or type 'cancel':");            
         }
     }
 
